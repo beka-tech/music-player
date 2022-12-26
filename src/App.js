aa2bed820ae402cd0931e436a91eb38bb99d2622
beka-tech <abeka039@gmail.com>
2022-12-26 15:53:31 +0300

    music app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import "./styles/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+     <Song/>
+     <Player/>
+    </div>
+  );
+}
+
+export default App;
